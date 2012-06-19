fe84fb3c97fda0df8345e2094fb49dd4b9f6a059
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-19 15:19:16 +0200

    Add some dummy sleep

--- src/main/java/eu/dnetlib/playground/PlaygroundStepJob.java
+++ src/main/java/eu/dnetlib/playground/PlaygroundStepJob.java
@@ -16,6 +16,12 @@ public class PlaygroundStepJob extends AbstractJobNode {
 	public void execute(Engine engine, NodeToken token) {
 		log.info("Executing node");
 		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
 		super.execute(engine, token);
 	}
 

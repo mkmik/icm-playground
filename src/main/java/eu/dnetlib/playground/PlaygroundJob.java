6d9767bffd87600354cea324af1bcb80dd043070
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-19 12:36:04 +0200

    Added graph

--- src/main/java/eu/dnetlib/playground/PlaygroundJob.java
+++ src/main/java/eu/dnetlib/playground/PlaygroundJob.java
@@ -2,13 +2,26 @@ package eu.dnetlib.playground;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.googlecode.sarasvati.Graph;
+
 import eu.dnetlib.enabling.tools.AbstractSchedulable;
 
 public class PlaygroundJob extends AbstractSchedulable {
 	private static final Log log = LogFactory.getLog(PlaygroundJob.class);
 
+	private Graph graph;
+
 	@Override
 	protected void doExecute() {
 		log.info("Started playing around");
+	}
+
+	public Graph getGraph() {
+		return graph;
+	}
+
+	public void setGraph(Graph graph) {
+		this.graph = graph;
 	}
 }

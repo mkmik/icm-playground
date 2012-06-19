27c81d88320dde2cf154e62a79da55115b476bfc
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-19 12:47:32 +0200

    Start workflow

--- src/main/java/eu/dnetlib/playground/PlaygroundJob.java
+++ src/main/java/eu/dnetlib/playground/PlaygroundJob.java
@@ -1,9 +1,14 @@
 package eu.dnetlib.playground;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.googlecode.sarasvati.Engine;
 import com.googlecode.sarasvati.Graph;
+import com.googlecode.sarasvati.GraphProcess;
+import com.googlecode.sarasvati.mem.MemGraphProcess;
 
 import eu.dnetlib.enabling.tools.AbstractSchedulable;
 
@@ -12,9 +17,20 @@ public class PlaygroundJob extends AbstractSchedulable {
 
 	private Graph graph;
 
+	/**
+	 * workflow engine.
+	 */
+	@Resource
+	private Engine engine;
+
 	@Override
 	protected void doExecute() {
-		log.info("Started playing around");
+		log.info("Starting job");
+
+		final GraphProcess process = new MemGraphProcess(graph);
+		engine.startProcess(process);
+
+		log.info("Job started");
 	}
 
 	public Graph getGraph() {
@@ -23,5 +39,13 @@ public class PlaygroundJob extends AbstractSchedulable {
 
 	public void setGraph(Graph graph) {
 		this.graph = graph;
+	}
+
+	public Engine getEngine() {
+		return engine;
+	}
+
+	public void setEngine(Engine engine) {
+		this.engine = engine;
 	}
 }

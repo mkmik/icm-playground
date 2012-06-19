bd28d7bc87666343a1d102006b8c1e33ca96c9e3
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2012-06-19 12:26:53 +0200

    Add node impl

---
+++ src/main/java/eu/dnetlib/playground/PlaygroundStepJob.java
@@ -0,0 +1,22 @@
+package eu.dnetlib.playground;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.googlecode.sarasvati.Engine;
+import com.googlecode.sarasvati.NodeToken;
+
+import eu.dnetlib.workflow.AbstractJobNode;
+
+public class PlaygroundStepJob extends AbstractJobNode {
+
+	private static final Log log = LogFactory.getLog(PlaygroundStepJob.class);
+	
+	@Override
+	public void execute(Engine engine, NodeToken token) {
+		log.info("Executing node");
+		
+		super.execute(engine, token);
+	}
+
+}
